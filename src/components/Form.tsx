ce0dd544d0e250b65a94bad885f9d38efb87aa09
Daniela <danielagp96@gmail.com>
2024-04-23 09:25:41 -0600

    FORMS ONE WAY DB:Adding a generic state for all the inputs in the form, the value in the inputs is the value in the state but I can not set this value in the browser, it says I need onchange

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
 import { categories } from "../data/categories";
 
 function Form() {
+  //NOTE - voy a crear el state, podira ser valido hacer esto
+  /*
+    const[category,setCategory] = useState();
+    const[activity,setActivity]= useState();
+    const[calories,setCalories] = useState();
+    
+    pero son muchos states y al final todos estan relacionados dependen uno del otro para calculos, puedo hacer mejor esto:
+  */
+ const[activity,setActivity] = useState({
+    category:1,
+    name:" ",
+    calories:0
+ });
+
   return (
     <form className="p-10 space-y-5 bg-white rounded-lg shadow-md">
       <section className="grid grid-cols-1 gap-3">
@@ -11,6 +26,9 @@ function Form() {
           id="category"
           name="category"
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+          //!SECTION por el momento esto es oneway data binding, no puedo modificar el state desde el input
+          //!SECTION para hacerlo necesito onchange
+          value={activity.category}
         >
           {categories.map((e) => (
             <option key={e.id} value={e.id}>
@@ -22,15 +40,16 @@ function Form() {
       </section>
       
       <section className="grid grid-cols-1 gap-3">
-        <label htmlFor="activity" className="font-bold">
+        <label htmlFor="name" className="font-bold">
           Actividad:
         </label>
         <input
           type="text"
-          name="activity"
-          id="activity"
+          name="name"
+          id="name"
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
           placeholder="EJ. comida, jugo de naranja, ensalada"
+          value={activity.name}
         />
       </section>
 
@@ -44,6 +63,7 @@ function Form() {
           id="calories"
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
           placeholder="EJ calorias. 200, 500"
+          value={activity.calories}
         />
       </section>
       
